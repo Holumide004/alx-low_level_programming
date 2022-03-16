9594f2d1d116b7036bea3b2047b56e1e30c2efe9
Holumide004 <olumideakinde77@gmail.com>
2022-03-16 03:38:25 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - print the phrase "Programming is like building a multilingual puzzle
  * Return: 0 of exited properly, non-zero otherwise
  */
-int main(void)	
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
- }
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
