47a4e0ba7f903ec8ac3f3397a9236a28c7d31723
Holumide004 <olumideakinde77@gmail.com>
2022-03-16 08:35:43 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;	
+	n = 0;
+
+	while (n < 10)
+	{
+		for (c = 'a'; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+	        _putchar('\n')
+		n++;
+	}
+}
