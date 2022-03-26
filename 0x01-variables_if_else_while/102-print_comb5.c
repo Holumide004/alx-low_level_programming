935d3a34b2790074187e71a166adf64e212b003f
Holumide004 <olumideakinde77@gmail.com>
2022-03-26 06:19:19 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (M = 0; m < 100; m++)
+	{
+		for (n = 0; n < 100; n++)
+		{
+			if (m < n)
+			{
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				piutchar(' ');
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				if (m != 98 || n != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
