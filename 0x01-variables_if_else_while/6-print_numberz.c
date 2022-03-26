aee86092b30683ae369e027d30b9af4021aeec18
Holumide004 <olumideakinde77@gmail.com>
2022-03-26 04:47:08 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
